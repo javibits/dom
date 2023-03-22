f7660483c5a7da4e534f4d911978d500cec3e45d
Javi CT <javibits.io@gmail.com>
2023-03-21 20:00:40 -0400

    Create app 'dom'. Add skeleton files

---
+++ dom/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "dom",
+    'summary': """
+        Doctor's Office Management
+    """,
+    'description': "",
+    'author': "javibits <javibits.io@gmail.com>",
+    'website': "",
+    'category': 'Medical Practice Management',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [
+    ],
+    'license': 'AGPL-3'
+}
---
+++ dom/models/models.py
@@ -0,0 +1,5 @@
+from odoo import models, fields, api
+
+
+class dom(models.Model):
+    pass
